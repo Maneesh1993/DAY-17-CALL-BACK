e31be9e0ab484fc59e96b06831ff0e0ccaf55762
Maneesh1993 <97601447+Maneesh1993@users.noreply.github.com>
2023-03-14 14:03:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// console.log("hi buddy")
+var a=document.createElement("h1")
+   setTimeout(()=>{
+    a.innerHTML=10
+        console.log("11111")
+        setTimeout(()=>{
+            a.innerHTML=9
+                console.log("11111")
+                setTimeout(()=>{
+                    a.innerHTML=8
+                        console.log("11111")
+                        setTimeout(()=>{
+                            a.innerHTML=7
+                                console.log("11111")
+                                setTimeout(()=>{
+                                    a.innerHTML=6
+                                        console.log("11111")
+                                        setTimeout(()=>{
+                                            a.innerHTML=5
+                                                console.log("11111")
+                                                setTimeout(()=>{
+                                                    a.innerHTML=4
+                                                        console.log("11111")
+                                                        setTimeout(()=>{
+                                                            a.innerHTML=3
+                                                                console.log("11111")
+                                                            },1000)
+                                                            setTimeout(()=>{
+                                                                a.innerHTML=2
+                                                                    console.log("11111")
+                                                                    setTimeout(()=>{
+                                                                        a.innerHTML=1
+                                                                            console.log("11111")
+                                                                            setTimeout(()=>{
+                                                                                a.innerHTML="Happy Independence day 😀😀😀"
+                                                                                    console.log("11111")
+                                                                                },1000)
+                                                                        },1000)
+                                                                },1000)
+                                                    },1000)
+                                            },1000)
+                                    },1000)
+                
+                            },1000)
+            
+                    },1000)
+            },1000)
+    },1000)
+
+    
+    a.className="align"
+      
+            
+                
+
+document.body.append(a)
+
